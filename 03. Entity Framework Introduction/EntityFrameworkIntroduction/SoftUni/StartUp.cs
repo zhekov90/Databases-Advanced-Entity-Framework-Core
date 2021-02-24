a10bb2bbed5d7038125569f180df86df02b1b1d4
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-24 18:33:50 +0200

    task added -  03. Employees Full Information

--- 03. Entity Framework Introduction/EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ 03. Entity Framework Introduction/EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace SoftUni
 {
-    class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            var softUniContext = new SoftUniContext();
+            var result = GetEmployeesFullInformation(softUniContext);
+            Console.WriteLine(result);
 
         }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                          .OrderBy(x => x.EmployeeId)
+                          .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
     }
+
 }
