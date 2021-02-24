5aa69f2b1d85bc7acb4567eedaa7326fb95b60b6
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-24 19:24:50 +0200

    files moved to upper directory

---
+++ 03. Entity Framework Introduction/SoftUni/StartUp.cs
@@ -0,0 +1,124 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var softUniContext = new SoftUniContext();
+
+            var result = AddNewAddressToEmployee(softUniContext);
+            Console.WriteLine(result);
+
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            context.Addresses.Add(address);
+            context.SaveChanges();
+
+            var nakov = context.Employees.FirstOrDefault(x => x.LastName == "Nakov");
+
+            nakov.AddressId = address.AddressId;
+            context.SaveChanges();
+
+            var addresses = context.Employees
+                .Select(x => new
+                {
+                    x.Address.AddressText,
+                    x.Address.AddressId
+                })
+                .OrderByDescending(x => x.AddressId)
+                .Take(10)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var currentAddress in addresses)
+            {
+                sb.AppendLine(currentAddress.AddressText);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x => new
+                {
+                    x.Salary,
+                    x.FirstName,
+                    x.LastName,
+                    DepartmentName = x.Department.Name
+                })
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                          .Where(x => x.Salary > 50000)
+                          .Select(x => new
+                          {
+                              x.FirstName,
+                              x.Salary,
+                          })
+                          .OrderBy(x => x.FirstName)
+                          .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                          .OrderBy(x => x.EmployeeId)
+                          .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+    }
+
+}
