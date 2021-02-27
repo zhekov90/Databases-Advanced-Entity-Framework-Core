301576f4afb7b12eec5af69dc7b04be2f839dabb
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-27 03:17:25 +0200

    add problem 1.	Student System

---
+++ 04. Entity Relations/P01_StudentSystem/Data/Configurations/ConnectionConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_StudentSystem.Data
+{
+    public static class ConnectionConfiguration
+    {
+        public const string CONNECTION_STRING =
+            @"Server=./Database=StudentSystem/Integrated Security = true";
+    }
+}
---
+++ 04. Entity Relations/P01_StudentSystem/Startup.cs
@@ -0,0 +1,15 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+   public class Startup
+    {
+       public static void Main(string[] args)
+        {
+            var context = new StudentSystemContext();
+            context.Database.EnsureCreated();
+
+        }
+    }
+}
