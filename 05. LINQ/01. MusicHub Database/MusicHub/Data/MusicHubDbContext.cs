c7c6fd1703f7eab2dda78af9e60a97b715599556
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-05 16:08:37 +0200

    resolve Problem 01. MusicHub Database

--- 05. LINQ/01. MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
+++ 05. LINQ/01. MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace MusicHub.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class MusicHubDbContext : DbContext
     {
@@ -13,6 +14,13 @@
         {
         }
 
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -24,7 +32,9 @@
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            
+            builder.Entity<SongPerformer>().HasKey(x => new { x.PerformerId, x.SongId });
+
+           
         }
     }
 }
