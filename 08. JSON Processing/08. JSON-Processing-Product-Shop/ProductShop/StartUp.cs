b204b3226d72b106e0a5800cfc431dc9f996e326
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-12 17:15:11 +0200

    add problem 01. Import Users

--- 08. JSON Processing/08. JSON-Processing-Product-Shop/ProductShop/StartUp.cs
+++ 08. JSON Processing/08. JSON-Processing-Product-Shop/ProductShop/StartUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AutoMapper;
 using Newtonsoft.Json;
 using ProductShop.Data;
 using ProductShop.Models;
@@ -12,7 +13,34 @@ namespace ProductShop
     {
         public static void Main(string[] args)
         {
+            ProductShopContext db = new ProductShopContext();
 
+            // ResetDatabase(db);
+
+            //Query 2. Import Users
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            var result = ImportUsers(db, inputJson);
+
+            Console.WriteLine(result);
         }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was successfully deleted!");
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was successfully created!");
+        }
+
     }
 }
