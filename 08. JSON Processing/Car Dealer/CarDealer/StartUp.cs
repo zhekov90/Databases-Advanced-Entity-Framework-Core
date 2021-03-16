78fd923b59e0dc84911ad6838ca176beb613f46d
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-16 03:04:58 +0200

    add problem 09. Import Suppliers

--- 08. JSON Processing/Car Dealer/CarDealer/StartUp.cs
+++ 08. JSON Processing/Car Dealer/CarDealer/StartUp.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
+using CarDealer.DTO;
 using CarDealer.Models;
 using Newtonsoft.Json;
 
@@ -13,6 +14,31 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+
+            var json = File.ReadAllText("../../../Datasets/suppliers.json");
+            ImportSuppliers(context, json);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersDtos = JsonConvert.DeserializeObject<IEnumerable<ImportSupplierInputModel>>(inputJson);
+
+            var suppliers = suppliersDtos.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
         }
     }
 }
