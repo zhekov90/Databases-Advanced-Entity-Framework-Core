8aef33779d9636a2414fe955f00fec9fd06061c8
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-19 03:59:26 +0200

    add problem 02. Import Products

--- 09. XML Processing/ProductShop/ProductShop/StartUp.cs
+++ 09. XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -2,14 +2,12 @@
 using ProductShop.Dtos.Import;
 using ProductShop.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml.Serialization;
-using AutoMapper;
-using System.Xml.Linq;
-using System.Xml;
 using CarDealer.XmlHelper;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using AutoMapper;
 
 namespace ProductShop
 {
@@ -23,15 +21,42 @@ namespace ProductShop
             context.Database.EnsureCreated();
 
             var usersXml = File.ReadAllText("../../../Datasets/users.xml");
+            var productsXml = File.ReadAllText("../../../Datasets/products.xml");
 
             //01.Import Users
-            var result = ImportUsers(context, usersXml);
+            ImportUsers(context, usersXml);
+
+            //02. Import Products
+            var result = ImportProducts(context, productsXml);
 
             Console.WriteLine(result);
         }
 
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            const string root = "Products";
+
+            var productDtos = XmlConverter.Deserializer<ProductImportModel>(inputXml, root);
+
+            var products = productDtos
+                .Select(x => new Product
+                {
+                    Name = x.Name,
+                    Price = x.Price,
+                    BuyerId = x.BuyerId,
+                    SellerId = x.SellerId
+                })
+                .ToArray();
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count()}";
+        }
+
         public static string ImportUsers(ProductShopContext context, string inputXml)
         {
+
             const string root = "Users";
 
             var userDtos = XmlConverter.Deserializer<UserImportModel>(inputXml, root);
