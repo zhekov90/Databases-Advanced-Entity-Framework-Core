8f493d56c285089d7a29fc81b6f2e9021db38bcc
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-19 04:13:43 +0200

    add problem 03. Import Categories

--- 09. XML Processing/ProductShop/ProductShop/StartUp.cs
+++ 09. XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -22,14 +22,47 @@ namespace ProductShop
 
             var usersXml = File.ReadAllText("../../../Datasets/users.xml");
             var productsXml = File.ReadAllText("../../../Datasets/products.xml");
+            var categoriesXml = File.ReadAllText("../../../Datasets/categories.xml");
 
             //01.Import Users
             ImportUsers(context, usersXml);
 
             //02. Import Products
-            var result = ImportProducts(context, productsXml);
+            ImportProducts(context, productsXml);
+
+            //03. Import Categories
+            var result = ImportCategories(context, categoriesXml);
 
             Console.WriteLine(result);
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            const string root = "Categories";
+
+            var categoriesDto = XmlConverter.Deserializer<CategoryImportModel>(inputXml, root);
+
+            List<Category> categories = new List<Category>();
+
+            foreach (var dto in categoriesDto)
+            {
+                if (dto==null)
+                {
+                    continue;
+                }
+
+                var category = new Category
+                {
+                    Name = dto.Name
+                };
+
+            categories.Add(category);
+            }
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
         }
 
         public static string ImportProducts(ProductShopContext context, string inputXml)
