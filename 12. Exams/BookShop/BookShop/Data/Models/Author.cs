d101ff81a0b8d8db0c6d96e209cbb25bb275716f
Zdravko Zhekov <hheelloo@abv.bg>
2021-03-26 19:09:21 +0200

    add Bookshop Exam

---
+++ 12. Exams/BookShop/BookShop/Data/Models/Author.cs
@@ -0,0 +1,25 @@
+﻿namespace BookShop.Data.Models
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class Author
+    {
+        public Author()
+        {
+            this.AuthorsBooks = new HashSet<AuthorBook>();
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
---
+++ 12. Exams/BookShop/BookShop/Data/Models/AuthorBook.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.Data.Models
+{
+    using Newtonsoft.Json;
+
+    public class AuthorBook
+    {
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
